bb30b1f59a12f78ca622ca0c5ea7517878d14a96
dbabichenko <dmb72@pitt.edu>
2026-01-29 16:22:52 -0500

    Update intro_to_sql.sql

--- class_examples_spring_2026/intro_to_sql.sql
+++ class_examples_spring_2026/intro_to_sql.sql
@@ -68,3 +68,58 @@ INSERT INTO registration (registration_id, fk_vehicle_id, registration_date)
 VALUES (uuid(), '33533231-fd54-11f0-ace5-fa52c99c4aae', CURDATE());
 
 SELECT * FROM registration;
+
+SET @vehicle_id = uuid();
+
+INSERT INTO vehicle (vehicle_id, make, model, prod_year, body_type)
+VALUES (@vehicle_id, 'Toyota', 'Camry', 2026, 'Sedan');
+
+INSERT INTO registration (registration_id, fk_vehicle_id, registration_date)
+VALUES (@vehicle_id, '33533231-fd54-11f0-ace5-fa52c99c4aae', CURDATE());
+
+
+
+USE classicmodels;
+
+SHOW tables;
+
+# Get all columns and all rows from table
+SELECT * FROM customers;
+
+# Get subset of columns
+SELECT customerNumber, customerName, phone, country FROM customers;
+
+# Get subset of rows
+SELECT customerNumber, customerName, phone, country FROM customers
+WHERE country = 'USA';
+
+SELECT customerNumber, customerName, phone, country FROM customers
+WHERE country = 'USA' AND customerName LIKE '%Toys%';
+
+# UPDATE STATEMENT
+USE dmb72;
+SELECT * FROM vehicle;
+
+ALTER TABLE vehicle
+ADD COLUMN color VARCHAR(20) NOT NULL;
+
+DESCRIBE vehicle;
+
+UPDATE vehicle SET color = 'blue';
+
+SELECT * FROM vehicle WHERE vehicle_id = '33533231-fd54-11f0-ace5-fa52c99c4aae';
+
+UPDATE vehicle SET color = 'red' WHERE vehicle_id = '33533231-fd54-11f0-ace5-fa52c99c4aae';
+
+UPDATE vehicle SET color = 'green', model = 'Expedition' 
+WHERE vehicle_id = '33533231-fd54-11f0-ace5-fa52c99c4aae';
+
+SELECT * FROM vehicle WHERE vehicle_id = '33533231-fd54-11f0-ace5-fa52c99c4aae';
+DELETE FROM vehicle  WHERE vehicle_id = '33533231-fd54-11f0-ace5-fa52c99c4aae';
+
+SELECT * FROM registration;
+
+INSERT INTO registration (registration_id, fk_vehicle_id, registration_date)
+VALUES (@vehicle_id, '33576851-fd54-11f0-ace5-fa52c99c4aae', CURDATE());
+
+DELETE FROM vehicle WHERE vehicle_id != '33576851-fd54-11f0-ace5-fa52c99c4aae';
